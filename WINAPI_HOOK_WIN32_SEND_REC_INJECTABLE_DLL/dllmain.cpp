5b61a344035ab2fa8b7cc065b1d3345a9f197bf1
vektorprime <vektorhalili@gmail.com>
2024-03-18 18:53:33 -0500

    fully working send and recv iat hooking

--- WINAPI_HOOK_WIN32_SEND_REC_INJECTABLE_DLL/dllmain.cpp
+++ WINAPI_HOOK_WIN32_SEND_REC_INJECTABLE_DLL/dllmain.cpp
@@ -2,46 +2,146 @@
 
 #include "pch.h"
 #include <windows.h>
+//winsock included in windows
+//#include <winsock.h>
 #include <winsock2.h>
-#include <winsock.h>
-#include <array>
-
-
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <iomanip> 
 
 #pragma comment(lib, "Ws2_32.lib")
+//#pragma comment(lib, "Wsock32.lib")
+
+std::ofstream log_file;
+
 
 /// Define the prototyps for function pointers
-typedef int(WINAPI *Prototypesend)(SOCKET s, const char* buf, int len, int flags);
-typedef int(WINAPI *Prototyperecv)(SOCKET s, char *buf,       int len, int flags);
+typedef int(WINAPI *Prototypesend)(SOCKET s, const char *buf, int len, int flags);
+typedef int(WINAPI *Prototyperecv)(SOCKET s, char *buf, int len, int flags);
 
-// Pointer to the original functions
-//Prototypesend originalsend = send;
-//Prototyperecv originalrecv = recv;
+////// Pointer to the original functions
+//Prototypesend original_send = send;
+//Prototyperecv original_recv = recv;
 
-Prototypesend originalsend;
-Prototyperecv originalrecv;
+
+Prototypesend original_send;
+Prototyperecv original_recv;
 
 // Hooked send function
-int hookedsend(SOCKET s, const char *buf, int len, int flags)
+int hooked_send(SOCKET s, const char *buf, int len, int flags)
 {
-    //modify the data and send
-    std::array<char, 8> newbuf = { 0 };
-    if (newbuf.at(4) == 0xd1 &&
-        newbuf.at(5) == 0x58)
+    char newbuf1[8];
+    bool bypass = false;
+
+    // Open the log file for writing
+    log_file.open("dll_log.txt", std::ios::app);
+    if (!log_file.is_open()) {
+        // Failed to open log file, exit
+        return FALSE;
+    }
+    log_file << "hooked_send triggered" << std::endl;
+    log_file << "packet sending is ";
+
+
+    for (int x = 0; x < len; ++x)
     {
-        newbuf.at(6) = 0x01;
+        log_file << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(static_cast<unsigned char>(buf[x])) << " ";
+    }
+    log_file << "\n";
+    ////modify the data and send
+
+    bool found_match = false;
+
+    if (len == 8)
+    {
+        log_file << "Looking at packet with 8 bytes" << std::endl;
+        if (buf[4] == static_cast<char>(0xd1)
+            && buf[5] == static_cast<char>(0x58)
+            && buf[6] == static_cast<char>(0x58)
+            && buf[7] == static_cast<char>(0x00))
+        {
+            log_file << "modifying packet to send skill 3 instead of 0" << std::endl;
+            for (int x = 0; x < len; ++x)
+            {
+                newbuf1[x] = buf[x];
+            }
+            newbuf1[6] = static_cast<char>(0x01);
+            bypass = true;
+        }
+       // {
+            //log_file << "comparing buf[4] with 0xd1" << std::endl;
+            //if (buf[4] == static_cast<char>(0xd1))
+            //{
+            //    log_file << "matched" << std::endl;
+
+            //    log_file << "comparing buf[5] with 0x58" << std::endl;
+            //    if (buf[5] == static_cast<char>(0x58))
+            //    {
+            //        log_file << "matched" << std::endl;
+
+            //        log_file << "comparing buf[6] with 0x18" << std::endl;
+            //        if (buf[6] == static_cast<char>(0x18))
+            //        {
+            //            log_file << "matched" << std::endl;
+
+            //            log_file << "comparing buf[7] with 0x00" << std::endl;
+            //            if (buf[7] == static_cast<char>(0x00))
+            //            {
+            //                log_file << "matched" << std::endl;
+            //                found_match = true;
+            //            }
+            //        }
+
+            //    }
+            //  
+            //}
+
+            //log_file << "checking found_match" << std::endl;
+
+           // if (found_match == true)
+            //{
+ 
+           // }
+    
+       // }
+
+    }
+    log_file.close();
+    if (bypass == true)
+    {
+        // Call the original send with modified data
+        return original_send(s, newbuf1, len, flags);
+    }
+    else
+    {
+        // Call the original send with unmodified data
+        return original_send(s, buf, len, flags);
     }
 
-    // Call the original send with the modified data
-    return originalsend(s, buf, len, flags);
 }
 
 // Hooked send function
-int hookedrecv(SOCKET s, char *buf, int len, int flags)
+int hooked_recv(SOCKET s, char *buf, int len, int flags)
 {
+    // Open the log file for writing
+    log_file.open("dll_log.txt", std::ios::app);
+    if (!log_file.is_open()) {
+        // Failed to open log file, exit
+        return FALSE;
+    }
+    log_file << "hooked_recv triggered" << std::endl;
+    log_file << "packet received is ";
+
+
+    for (int x = 0; x < len; ++x)
+    {
+        log_file << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(static_cast<unsigned char>(buf[x])) << " ";
+
+    }
+    log_file << "\n";
+  
     ////modify the data and send
     //std::array<char, 8> newbuf = { 0 };
     //for (int x = 0; x < len; ++x)
@@ -52,13 +152,27 @@ int hookedrecv(SOCKET s, char *buf, int len, int flags)
     //        newbuf.at(6) = 0x0b;
     //    }
     //}
-
+   // MessageBoxW(NULL, L"HIT RECV", L"HIT RECV", 0);
     // Call the original send with the modified data
-    return originalrecv(s, buf, len, flags);
+    log_file.close();
+    return original_recv(s, buf, len, flags);
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 {
+
+
+    ///logging
+    // Open the log file for writing
+    log_file.open("dll_log.txt", std::ios::app);
+    if (!log_file.is_open()) {
+        // Failed to open log file, exit
+        return FALSE;
+    }
+
+    log_file << "log started\n";
+
+    ///other
     HMODULE hModule = 0;
     DWORD oldProtect = 0;
 
@@ -73,18 +187,28 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
     switch (fdwReason)
     {
          case DLL_PROCESS_ATTACH:
+   
+
+             // Output initialization message to log file
+             log_file << "attached\n";
+
              /////////        /////////        /////////        /////////
              // Get the address of the send function
-             originalsend = (Prototypesend)GetProcAddress(GetModuleHandle(TEXT("ws2_32.dll")), "send");
-             if (originalsend == NULL)
+             original_send = (Prototypesend)GetProcAddress(GetModuleHandle(L"ws2_32.dll"), "send");
+             //original_send = (Prototypesend)GetProcAddress(GetModuleHandle(L"wsock32.dll"), "send");
+             log_file << "original_send func address is " << original_send << std::endl;
+
+             if (original_send == NULL)
              {
                  // Error handling
                  return FALSE;
              }
 
              // Get the address of the recv function
-             originalrecv = (Prototyperecv)GetProcAddress(GetModuleHandle(TEXT("wsock32.dll")), "recv");
-             if (originalrecv == NULL)
+             original_recv = (Prototyperecv)GetProcAddress(GetModuleHandle(L"wsock32.dll"), "recv");
+             log_file << "original_recv func address is " << original_recv << std::endl;
+
+             if (original_recv == NULL)
              {
                  // Error handling
                  return FALSE;
@@ -102,32 +226,45 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 
             // Get the address of the IMAGE_DOS_HEADER
             pDosHeader = (PIMAGE_DOS_HEADER)hModule;
+            log_file << "pDosHeader address is " << pDosHeader << std::endl;
 
             // Get the address of the IMAGE_NT_HEADERS
             pNtHeader = (PIMAGE_NT_HEADERS)((DWORD_PTR)hModule + pDosHeader->e_lfanew);
+            log_file << "pNtHeader address is " << pNtHeader << std::endl;
 
             // Get the address of the IMAGE_IMPORT_DESCRIPTOR
             pImportDesc = (PIMAGE_IMPORT_DESCRIPTOR)((DWORD_PTR)hModule + pNtHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
+            log_file << "pImportDesc address is " << pImportDesc << std::endl;
 
             // Iterate over each imported module
             while (pImportDesc->Name != NULL)
             {
-                // Get the name of the imported module
-                LPCSTR szModuleName = (LPCSTR)((DWORD_PTR)hModule + pImportDesc->Name);
+                log_file << "Iterating over pImportDesc" << std::endl;
 
+                // Get the name of the imported module
+              //  LPCWSTR szModuleName = reinterpret_cast<LPCWSTR>((DWORD_PTR)hModule + pImportDesc->Name);
+                LPCSTR szModuleName = reinterpret_cast<LPCSTR>((DWORD_PTR)hModule + pImportDesc->Name);
+                log_file << "szModuleName is " << szModuleName << std::endl;
                 // Check if the imported module is kernel32.dll (just as an example)
-                if (strcmp(szModuleName, "ws2_32.dll") == 0)
+                std::string module_name_lower(szModuleName);
+                std::transform(module_name_lower.begin(), module_name_lower.end(), module_name_lower.begin(), ::tolower);
+                if (module_name_lower == "ws2_32.dll")
                 {
+                    log_file << "Found ws2_32.dll" << std::endl;
                     // Get the address of the thunk array
                     PIMAGE_THUNK_DATA pThunk = (PIMAGE_THUNK_DATA)((DWORD_PTR)hModule + pImportDesc->FirstThunk);
-
+                    log_file << "ws2_32 pThunk array address is " << pThunk << std::endl;
+                    log_file << "original_send func address is " << original_send << std::endl;
                     // Iterate over each function in the import address table (IAT)
-                    while (pThunk->u1.Function != NULL)
+                    bool send_found = false;
+                    while (send_found == false)
                     {
-
+                        log_file << "pThunk->u1.Function address: " << (DWORD_PTR)pThunk->u1.Function << std::endl;
                         //send replace
-                        if ((DWORD_PTR)pThunk->u1.Function == (DWORD_PTR)originalsend)
+                        if ((DWORD_PTR)pThunk->u1.Function == (DWORD_PTR)original_send)
                         {
+                            send_found = true;
+                            log_file << "Found original_send in the thunk array" << std::endl;
                             // Modify the memory protection of the page
                             DWORD dwOldProtect;
                             if (!VirtualProtect(&pThunk->u1.Function, sizeof(DWORD_PTR), PAGE_READWRITE, &dwOldProtect))
@@ -135,30 +272,41 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
                                 // Error handling
                                 return 1;
                             }
+                            log_file << "Set the memory area of the IAT to PAGE_READWRITE" << std::endl;
 
                             // Hook the function by replacing its address with the address of our hooked function
-                            pThunk->u1.Function = (DWORD_PTR)hookedsend;
+                            pThunk->u1.Function = (DWORD_PTR)hooked_send;
+                            log_file << "Wrote the hooked_recv in the IAT" << std::endl;
 
                             // Restore the original memory protection
                             VirtualProtect(&pThunk->u1.Function, sizeof(DWORD_PTR), dwOldProtect, &dwOldProtect);
+                            log_file << "Set the memory area of the IAT back to protected" << std::endl;
 
                             // Move to the next thunk
-                            ++pThunk;
                         }
+                        ++pThunk;
+
                     }
                 }
-                if (strcmp(szModuleName, "wsock32.dll") == 0)
+                if (module_name_lower == "wsock32.dll")
                 {
+                    log_file << "Found wsock32.dll" << std::endl;
                     // Get the address of the thunk array
                     PIMAGE_THUNK_DATA pThunk = (PIMAGE_THUNK_DATA)((DWORD_PTR)hModule + pImportDesc->FirstThunk);
-
+                    log_file << "wsock32 pThunk array address is " << pThunk << std::endl;
+                    log_file << "original_recv func address is " << original_recv << std::endl;
                     // Iterate over each function in the import address table (IAT)
-                    while (pThunk->u1.Function != NULL)
+                    bool recv_found = false;
+                    while (recv_found == false)
                     {
+                        log_file << "pThunk->u1.Function address: " << (DWORD_PTR)pThunk->u1.Function << std::endl;
 
                         //recv replace
-                        if ((DWORD_PTR)pThunk->u1.Function == (DWORD_PTR)originalrecv)
+                        if ((DWORD_PTR)pThunk->u1.Function == (DWORD_PTR)original_recv)
                         {
+                            recv_found = true;
+                            log_file << "Found original_recv in the thunk array" << std::endl;
+
                             // Modify the memory protection of the page
                             DWORD dwOldProtect;
                             if (!VirtualProtect(&pThunk->u1.Function, sizeof(DWORD_PTR), PAGE_READWRITE, &dwOldProtect))
@@ -166,27 +314,36 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
                                 // Error handling
                                 return 1;
                             }
+                            log_file << "Set the memory area of the IAT to PAGE_READWRITE" << std::endl;
 
                             // Hook the function by replacing its address with the address of our hooked function
-                            pThunk->u1.Function = (DWORD_PTR)hookedrecv;
+                            pThunk->u1.Function = (DWORD_PTR)hooked_recv;
+
+                            log_file << "Wrote the hooked_recv in the IAT" << std::endl;
 
                             // Restore the original memory protection
                             VirtualProtect(&pThunk->u1.Function, sizeof(DWORD_PTR), dwOldProtect, &dwOldProtect);
 
-                            // Move to the next thunk
-                            ++pThunk;
+                            log_file << "Set the memory area of the IAT back to protected" << std::endl;
                         }
+                        // Move to the next thunk
+                        ++pThunk;
                     }
                 }
                     // Move to the next imported module
                     ++pImportDesc;
             }
+
+            log_file << "ALL HOOKS SET!" << std::endl;
+
             break;
         case DLL_THREAD_ATTACH:
             break;
         case DLL_THREAD_DETACH:
             break;
         case DLL_PROCESS_DETACH:
+            // Close the log file
+            log_file.close();
             break;
     }
 
